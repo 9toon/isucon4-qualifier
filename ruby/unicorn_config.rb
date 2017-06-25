b7c179a2136c761d086f60d12e2e5ad4c4fc2a75
Yusuke Naito <yusuke-naito@cookpad.com>
2017-06-25 16:25:25 +0900

    Turn off unicorn logs

--- ruby/unicorn_config.rb
+++ ruby/unicorn_config.rb
@@ -1,8 +1,8 @@
 @dir = "/home/isucon/webapp/ruby/"
 working_directory @dir
 
-stderr_path File.join(@dir, "log/unicorn.stderr.log")
-stdout_path File.join(@dir, "log/unicorn.stdout.log")
+# stderr_path File.join(@dir, "log/unicorn.stderr.log")
+# stdout_path File.join(@dir, "log/unicorn.stdout.log")
 
 worker_processes 10
 preload_app true
