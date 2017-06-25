8f44da5ddc55a8531464b4438facd7f7f0745044
Yusuke Naito <yusuke-naito@cookpad.com>
2017-06-25 23:27:45 +0900

    Reset redis data

--- ruby/config/init.sh
+++ ruby/config/init.sh
@@ -15,3 +15,6 @@ mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_log.sql
 # index
 mysql -h ${myhost} -P ${myport} -u ${myuser} -e 'create index `idx_login_log_user_id_succeeded` on login_log (`user_id`, `succeeded`);'  ${mydb}
 mysql -h ${myhost} -P ${myport} -u ${myuser} -e 'create index `idx_login_log_ip_succeeded` on login_log (`ip`, `succeeded`);'  ${mydb}
+
+# redis
+redis-cli flushall
